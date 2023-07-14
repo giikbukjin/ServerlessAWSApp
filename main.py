5653792ebf85a67a998cae4f8ea71f16a30abdaa
Jinseo Jeong <104565869+JinNitt@users.noreply.github.com>
2023-07-14 11:51:49 +0900

    [FEAT] Tab 기능 구현
    Gradio의 Tab 기능을 이용하여 조회, 입력, 수정, 삭제 기능을 각각의 페이지에서 수행할 수 있도록 구현

--- main.py
+++ main.py
@@ -5,70 +5,97 @@ import pandas as pd
 csv_file = "AccountBook.csv"
 
 # 데이터 조회 함수
-def read():
+def read_fn():
     df = pd.read_csv(csv_file)
     return df
 
 # 데이터 입력 함수
-def create(date, description, amount, category, memo):
-    df = read()
-    entry = pd.DataFrame({"날짜": [date], "항목": [description], "금액": [amount], "카테고리": [category], "메모": [memo]})
+def create_fn(div, date, description, amount, category, memo):
+    df = read_fn()
+    entry = pd.DataFrame({"분류": [div], "날짜": [date], "항목": [description], "금액": [amount], "카테고리": [category], "메모": [memo]})
     df = pd.concat([df, entry], ignore_index = True)
     write(df)
-    return df
+    return "입력이 완료되었습니다."
 
 def write(df):
     df.to_csv(csv_file, index = False)
 
 # 데이터 수정 함수
-def update(index, date, description, amount, category, memo):
-    df = read()
+def update_fn(index, div, date, description, amount, category, memo):
+    df = read_fn()
     if index < len(df):
-        df.loc[index] = [date, description, amount, category, memo]
+        df.loc[index] = [div, date, description, amount, category, memo]
         write(df)
-    return df
+    return "수정이 완료되었습니다."
 
 # 데이터 삭제 함수
-def delete(index):
-    df = read()
+def delete_fn(index):
+    df = read_fn()
     if index < len(df):
         df = df.drop(index = index)
         df.reset_index(drop = True, inplace = True)
         write(df)
-    return df
+    return "삭제가 완료되었습니다."
 
 
-# Gradio 인터페이스
-def gradio_interface(action, date, description, amount, category, memo, index = None):
-    if action == "조회":
-        return read()
-    elif action == "입력":
-        if date and description and amount:
-            create(date, description, amount, category, memo)
-        return read()
-    elif action == "수정":
-        update(index, date, description, amount, category, memo)
-        return read()
-    elif action == "삭제":
-        delete(index)
-        return read()
+# Gradio UI 인터페이스
+with gr.Blocks() as demo:
+    gr.Markdown("가계부 프로그램")
 
-# Gradio UI 구성 요소
-inputs = [
-    gr.inputs.Dropdown(choices = ["조회", "입력", "수정", "삭제"], label = "동작"),
-    gr.inputs.Textbox(label = "날짜"), 
-    gr.inputs.Textbox(label = "항목"),
-    gr.inputs.Textbox(label = "금액"),
-    gr.inputs.Dropdown(choices = ["수입", "식비", "교통비", "통신비", "주거비", "문화비", "저축", "기타"], label = "카테고리"),
-    gr.inputs.Textbox(label = "메모"),
-    gr.inputs.Number(label = "인덱스 (수정 또는 삭제할 때만 필요)")
-]
+    with gr.Tab("조회"):
+        read_input = None
+        read_output = gr.outputs.Dataframe(type = "pandas")
+        read_button = gr.Button("저장")
 
-outputs = gr.outputs.Dataframe(type = "pandas")  # DataFrame 출력 요소
+    with gr.Tab("입력"):
+        create_input = [
+            gr.inputs.Radio(choices = ["수입", "지출"], label = "동작"),
+            gr.inputs.Textbox(label = "날짜"), 
+            gr.inputs.Textbox(label = "항목"),
+            gr.inputs.Textbox(label = "금액"),
+            gr.inputs.Dropdown(choices = ["수입", "식비", "교통비", "통신비", "주거비", "문화비", "저축", "기타"], label = "카테고리"),
+            gr.inputs.Textbox(label = "메모"),
+        ]
+        create_output = gr.Textbox(label = "결과")
+        create_button = gr.Button("저장")
 
-gr.Interface(
-    fn = gradio_interface,
-    inputs = inputs,
-    outputs = outputs,
-    title = "가계부 프로그램"
-).launch(share = True)
+    with gr.Tab("수정"):
+        update_input = [
+            gr.inputs.Radio(choices = ["수입", "지출"], label = "동작"),
+            gr.inputs.Textbox(label = "날짜"), 
+            gr.inputs.Textbox(label = "항목"),
+            gr.inputs.Textbox(label = "금액"),
+            gr.inputs.Dropdown(choices = ["수입", "식비", "교통비", "통신비", "주거비", "문화비", "저축", "기타"], label = "카테고리"),
+            gr.inputs.Textbox(label = "메모"),
+            gr.inputs.Number(label = "인덱스(0부터 시작)")
+        ]
+        update_output = gr.Textbox(label = "결과")
+        update_button = gr.Button("저장")
+
+    with gr.Tab("삭제"):
+        delete_input =  gr.inputs.Number(label = "인덱스")
+        delete_output = gr.Textbox(label = "결과")
+        delete_button = gr.Button("저장")
+
+
+    read_button.click(
+        fn = read_fn, 
+        inputs = read_input, 
+        outputs = read_output
+    )
+    create_button.click(
+        fn = create_fn, 
+        inputs = create_input, 
+        outputs = create_output
+    )
+    update_button.click(
+        fn = update_fn, 
+        inputs = update_input, 
+        outputs = update_output
+    )
+    create_button.click(
+        fn = delete_fn, 
+        inputs = delete_input, 
+        outputs = delete_output
+    )
+demo.launch()
